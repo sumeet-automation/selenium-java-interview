3bf6176dd6c577a22d377393b3d952488ed62cdb
Sumeet Agrawal <sumeetagrawal840@gmail.com>
2023-08-12 23:06:13 +0530

    Add files via upload
    Question 61 added

---
+++ src/Question61.java
@@ -0,0 +1,9 @@
+public class Question61 {
+
+    public static void main(String[] args) {
+
+        String input = "I  live   in   Bengaluru   ";
+        input = input.replaceAll("\\s+", " ").strip();
+        System.out.println(input);
+    }
+}
