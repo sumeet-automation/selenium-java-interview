53a818d73138b94ba64bad944435cfdd32cc1d9d
Sumeet Agrawal <sumeetagrawal840@gmail.com>
2024-01-06 20:43:11 +0530

    Update Question63 with more simpler logics

--- src/complex/Question63.java
+++ src/complex/Question63.java
@@ -3,8 +3,29 @@ import java.util.stream.IntStream;
 public class Question63 {
 
     public static void main(String[] args) {
+        //Withoug Streams
+        for(int i=10;i<100;i++)
+        printNumber(i);
+
+        for (int i = 10; i < 100; i++)
+            if (printNumber2(i))
+                System.out.println(i);
+        
+        //Using Streams
         IntStream.range(10, 100)
                 .filter(x -> (x / 10) + (x % 10) == 10)
                 .forEach(System.out::println);
+    }
+
+    public static void printNumber(int number) {
+        String check = String.valueOf(number);
+        String numbers[] = check.split("");
+        if (Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]) == 10) {
+            System.out.println(number);
+        }
+    }
+
+    public static boolean printNumber2(int number) {
+        return (number / 10) + (number % 10) == 10;
     }
 }
